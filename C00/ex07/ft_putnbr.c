516ff3e59a9764bef0cae827f7e51ae0c22c697e
Iana Kozlova <ikozlova@e3r2p3.42roma.it>
2023-09-25 17:29:49 +0200

    ex07

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikozlova <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 15:23:59 by ikozlova          #+#    #+#             */
+/*   Updated: 2023/09/25 17:24:07 by ikozlova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	printdigit(int num)
+{
+	if (num == 0)
+	{
+		return ;
+	}
+	printdigit(num / 10);
+	ft_putchar('0' + (num % 10));
+}
+
+void	ft_putnbr(int num)
+{
+	if (num == 0)
+	{
+		ft_putchar('0');
+		return ;
+	}
+	if (num < 0)
+	{
+		ft_putchar('-');
+		num = -num;
+	}
+	printdigit(num);
+}
